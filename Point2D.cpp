5d456d2686a76c1a6015e9e08109ec0c2311ed99
engineerIOT <46654184+engineerIOT@users.noreply.github.com>
2020-09-16 23:03:11 -0300

    Add files via upload

--- Point2D.cpp
+++ Point2D.cpp
@@ -13,6 +13,7 @@ Point2D::Point2D(double x, double y) {
 void Point2D::setX(double x) { 
 	xVal = x; 
 }
+
 void Point2D::setY(double y) { 
 	yVal = y; }
 
@@ -26,27 +27,33 @@ double Point2D::y() const {
 
 void Point2D::testePoint2D() {
 
-	Point2D alpha;
-	Point2D beta;
-	Point2D* ptr;
+	//1. criar instancia para dois objetos
+	//2. criar ponteiro
+	//3. atribuir valor
+	//4. printar
+	//5. zerar ponteiro
 
-	double x1 = 1.1;
-	double y1 = 2.5;
-	Point2D(x1, y1);
+	Point2D alpha; //criando instancia para um objeto do tipo Point2D
+	Point2D beta;  //criando instancia para um objeto do tipo Point2D
+	Point2D* ptr;  //criando ponteiro para um objeto do tipo Point2D
+
+	double x1 = 1.1;	//atribuindo valor para x1
+	double y1 = 2.5;	//atribuindo valor para y1
+	Point2D(x1, y1);	//atribuindo valor para o construtor
 
 
-	ptr = &alpha;
-	ptr->setX(x1);
-	ptr->setY(y1);
+	ptr = &alpha;		//
+	ptr->setX(x1);		//
+	ptr->setY(y1);		//
 
 
-	double x2 = 5.3;
-	double y2 = 6.5;
-	Point2D(x2, y2);
+	double x2 = 5.3;	//atribuindo valor para x2
+	double y2 = 6.5;	//atribuindo valor para y2
+	Point2D(x2, y2);	//atribuindo valor para o construtor
 
-	ptr = &beta;
-	ptr->setX(x2);
-	ptr->setY(y2);
+	ptr = &beta;		//
+	ptr->setX(x2);		//
+	ptr->setY(y2);		//
 
 	cout << "Endereco:" << &alpha << " x1:" << x1 << "" << " y1:" << y1 << endl;
 	cout << "Endereco:" << &beta << " x2:" << x2 << "" << " y2:" << y2 << endl;
